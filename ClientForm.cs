d44430455ff7b416561fb07a9063fa63587cbb51
HaosNayt <31624877+HaosNayt@users.noreply.github.com>
2022-12-22 12:20:13 +0300

    Add files via upload

---
+++ ClientForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace salonKrasoti
+{
+    public partial class ClientForm : Form
+    {
+
+        public ClientForm()
+        {
+
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=323-1\SQLEXPRESS;Initial Catalog=salonKrasoti; Integrated Security=True");
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("INSER INTO Предстоящие записи  values ("+dateTimePicker1.Value+"," +comboBox1.Text+",)", con);
+
+
+
+        }
+
+        private void ClientForm_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=323-1\SQLEXPRESS;Initial Catalog=salonKrasoti; Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Услуги", con);
+            SqlDataReader reader = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
--- SignIn.Designer.cs
+++ SignIn.Designer.cs
@@ -1,17 +1,14 @@
 ﻿namespace salonKrasoti
 {
-    partial class Form1
+    partial class SignIn
     {
-        /// <summary>
-        /// Обязательная переменная конструктора.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Освободить все используемые ресурсы.
-        /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
-        protected override void Dispose(bool disposing)
+
+
+
+
+       protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
             {
@@ -28,13 +25,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SignIn));
+            this.signInButton = new System.Windows.Forms.Button();
+            this.loginInput = new System.Windows.Forms.TextBox();
+            this.passwordInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // signInButton
+            // 
+            this.signInButton.Location = new System.Drawing.Point(121, 195);
+            this.signInButton.Name = "signInButton";
+            this.signInButton.Size = new System.Drawing.Size(75, 23);
+            this.signInButton.TabIndex = 0;
+            this.signInButton.Text = "Войти";
+            this.signInButton.UseVisualStyleBackColor = true;
+            this.signInButton.Click += new System.EventHandler(this.signInButton_Click);
+            // 
+            // loginInput
+            // 
+            this.loginInput.Location = new System.Drawing.Point(106, 85);
+            this.loginInput.Name = "loginInput";
+            this.loginInput.Size = new System.Drawing.Size(100, 20);
+            this.loginInput.TabIndex = 1;
+            // 
+            // passwordInput
+            // 
+            this.passwordInput.Location = new System.Drawing.Point(106, 126);
+            this.passwordInput.Name = "passwordInput";
+            this.passwordInput.Size = new System.Drawing.Size(100, 20);
+            this.passwordInput.TabIndex = 2;
+            // 
+            // SignIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(346, 311);
+            this.Controls.Add(this.passwordInput);
+            this.Controls.Add(this.loginInput);
+            this.Controls.Add(this.signInButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SignIn";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.SignIn_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button signInButton;
+        private System.Windows.Forms.TextBox loginInput;
+        private System.Windows.Forms.TextBox passwordInput;
     }
 }
 
